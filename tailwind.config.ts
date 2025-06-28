d735abf307e3706f3e430958ad981f315397a7e5
charan23-ai <kcharanreddy07@gmail.com>
2025-06-28 12:08:22 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./main.tsx/**/*.{ts,tsx}",
 	],
 	prefix: "",
 	theme: {
