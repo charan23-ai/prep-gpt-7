9157d2a5bd17e09d5ed7821312dd99ffdd7e3e51
charan23-ai <kcharanreddy07@gmail.com>
2025-07-01 21:35:33 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+ root : './src',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
